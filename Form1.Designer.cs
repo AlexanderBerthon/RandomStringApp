9ce26aaa87bc2a40dcf64821096e50f5a9ee27e0
Alex Berthon <Alexjberthon@gmail.com>
2023-03-09 15:56:30 -0800

    Created new functionality to allow the user to specify the length of the string to generate
    Added UI elements to match

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -23,36 +23,77 @@
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.generateBtn = new System.Windows.Forms.Button();
+            this.lengthSelect = new System.Windows.Forms.NumericUpDown();
+            this.lengthLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.lengthSelect)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // outputTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 50);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(160, 23);
-            this.textBox1.TabIndex = 0;
+            this.outputTextBox.Location = new System.Drawing.Point(12, 31);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(160, 23);
+            this.outputTextBox.TabIndex = 0;
             // 
-            // button1
+            // generateBtn
             // 
-            this.button1.Location = new System.Drawing.Point(53, 79);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Generate";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.generateBtn.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.generateBtn.Location = new System.Drawing.Point(53, 105);
+            this.generateBtn.Name = "generateBtn";
+            this.generateBtn.Size = new System.Drawing.Size(75, 23);
+            this.generateBtn.TabIndex = 1;
+            this.generateBtn.Text = "Generate";
+            this.generateBtn.UseVisualStyleBackColor = true;
+            this.generateBtn.Click += new System.EventHandler(this.generateBtn_Click);
+            // 
+            // lengthSelect
+            // 
+            this.lengthSelect.Font = new System.Drawing.Font("Consolas", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lengthSelect.Location = new System.Drawing.Point(103, 65);
+            this.lengthSelect.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.lengthSelect.Minimum = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.lengthSelect.Name = "lengthSelect";
+            this.lengthSelect.Size = new System.Drawing.Size(43, 25);
+            this.lengthSelect.TabIndex = 2;
+            this.lengthSelect.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.lengthSelect.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.lengthSelect.ValueChanged += new System.EventHandler(this.lengthSelect_ValueChanged);
+            // 
+            // lengthLabel
+            // 
+            this.lengthLabel.AutoSize = true;
+            this.lengthLabel.Font = new System.Drawing.Font("Consolas", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lengthLabel.Location = new System.Drawing.Point(41, 68);
+            this.lengthLabel.Name = "lengthLabel";
+            this.lengthLabel.Size = new System.Drawing.Size(56, 17);
+            this.lengthLabel.TabIndex = 3;
+            this.lengthLabel.Text = "Length";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(184, 161);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lengthLabel);
+            this.Controls.Add(this.lengthSelect);
+            this.Controls.Add(this.generateBtn);
+            this.Controls.Add(this.outputTextBox);
             this.Name = "Form1";
-            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.lengthSelect)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -60,7 +101,9 @@
 
         #endregion
 
-        private TextBox textBox1;
-        private Button button1;
+        private TextBox outputTextBox;
+        private Button generateBtn;
+        private NumericUpDown lengthSelect;
+        private Label lengthLabel;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,13 @@ using System;
 using System.Security.Cryptography;
 
 namespace RandomStringApp {
+    /// <summary>
+    /// This simple program generates a string by randomly selecting characters from the CharPool array and at a length determined by the user
+    /// </summary>
     public partial class Form1 : Form {
 
-        char[] options = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+        //Pool of characters to pick from
+        char[] CharPool = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A',
             'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
             'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5',
@@ -12,20 +16,28 @@ namespace RandomStringApp {
             '(', ')', '-', '_', '=', '+', '[', '{', ']', '}', '\\', '|', ',', '<', '.',
             '>', '/', '?', ';', ':', '\'', '\"'};
 
+        //variable to control length of output string
+        int length = 8;
+        
         public Form1() {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e) {
+        private void lengthSelect_ValueChanged(object sender, EventArgs e) {
+            length = (int)lengthSelect.Value;
+        }
+
+        private void generateBtn_Click(object sender, EventArgs e) {
             Random rng = new Random();
             String output = "";
 
-            for (int i = 0; i < 9; i++) {
-                int randomIndex = rng.Next(options.Length);
-                output += options[randomIndex];
+            //'Randomly' select character from pool and add to string
+            for (int i = 0; i < this.length; i++) {
+                int randomIndex = rng.Next(CharPool.Length);
+                output += CharPool[randomIndex];
             }
 
-            textBox1.Text = output;
+            outputTextBox.Text = output;
         }
     }
 }
