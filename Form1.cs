baa5f67926244fbfb59e365240ab83518d1c3254
Alex Berthon <Alexjberthon@gmail.com>
2023-03-17 16:35:07 -0700

    Whitespace fix, probability testing, and cleanup
    Created logic to prevent whitespace from occuring at the beginning or the end of the randomly generated string
    
    Balanced the representation of each character pool to be about equal to eachother. Still not entirely sure if this is a good idea.
    
    Cleaned up test code and math caclulation functions
    
    *should have probably broken these up into different commits tbh

--- Form1.cs
+++ Form1.cs
@@ -3,18 +3,23 @@ using System.Security.Cryptography;
 
 /*
 improvements
- - kind of bad to have a string start or end with whitespace
  - whitespace is only 1 character in the array. the chance of it showing up in a random string
-could be as low as.. 8.42% if all options are selected. Is that a problem? not enough representation?
+could be as low as.. 8.12% if all options are selected. Is that a problem? not enough representation?
 
 here is a representation chart for each option (again based on all options selected)
 
--chance at least 1 character from list is represented in random 8 char string (all options selected)-
-alphaLower = 
-alphaUpper = 
-symbol     = 
-whitespace =  
-numeric    = 
+chance at least 1 character from list is represented in random 8 char string (all options selected)-
+alphaLower = 92.26% representation
+alphaUpper = 92.26% representation
+symbol     = 96.26% representation
+whitespace = 08.12% representation 
+numeric    = 58.93% representation
+
+should all sets be equal in probability? 
+there is a representation skew specifically around whitespace and numeric
+could be solved by duplicating data within these pools until they are around the same size as the other pools
+but is that a good idea?
+
 */
 
 namespace RandomStringApp {
@@ -32,13 +37,16 @@ namespace RandomStringApp {
         List<char> alphaUpper = new List<char>{'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
             'M', 'N', 'O', 'P','Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
 
-        char whiteSpace = ' ';
+        List<char> whiteSpace = new List<char>{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
+             ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
 
         List<char> symbols = new List<char>{'~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(',
             ')', '-', '_', '=', '+', '[', '{', ']', '}', '\\', '|', ',', '<', '.',
             '>', '/', '?', ';', ':', '\'', '\"' };
         
-        List<char> numeric = new List<char>() { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+        List<char> numeric = new List<char>() { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
 
         //variable to control length of output string
         int length = 8;
@@ -75,7 +83,7 @@ namespace RandomStringApp {
             }
 
             if (space_checkBox.Checked) {
-                CharPool.Add(whiteSpace);
+                CharPool.AddRange(whiteSpace);
             }
 
             //'Randomly' select character from pool and add to string
@@ -83,38 +91,25 @@ namespace RandomStringApp {
                 errorLabel.Visible = true;
             }
             else {
-                int testcount = 0;
-                for (int t = 0; t < 100000; t++) {
-                    for (int i = 0; i < this.length; i++) {
-                        int randomIndex = rng.Next(CharPool.Count());
+                int randomIndex = 0;
+                
+                for (int i = 0; i < this.length; i++) {
+                    //case included to prevent string beginning or ending in a whitespace
+                    if(i == 0 || i == this.length - 1) {
+                        char noSpace = ' ';
+                        while(noSpace == ' ') {
+                            randomIndex = rng.Next(CharPool.Count());
+                            noSpace = CharPool[randomIndex];
+                        }
+                    }
+                    else {
+                        randomIndex = rng.Next(CharPool.Count());
+                    }
                         output += CharPool[randomIndex];
-                    }
-                    if(output.Contains(" ")){
-                        testcount++;
-                    }
-                    output = "";
                 }
-                outputTextBox.Text = testcount.ToString();
             }
-
+            outputTextBox.Text = output;
             CharPool.Clear(); //clears out all data for next generation
         }
-
-
-        //I don't want to make an interaction function for every single check box
-        //because I would be repeating some code like 6 times
-        //but combining all the logic in one function is very messy
-        //both options are bad
-        //
-
-        //why should I do work when the user clicks the box?
-        //what if the user clicks the box 10 times?
-        //then I am modifying the list 10 times
-        //that's stupid
-        //instead, when the user clicks the generate button, check all the boxes and make ONE modification
-        //
-
-
-
     }
 }
