1f086122cee035b4b7576ed4cbc6348aae0da350
Alex Berthon <Alexjberthon@gmail.com>
2023-03-20 14:58:24 -0700

    Clean up cleaned up comments and simplified documentation.
    Moved comments and thoughts to readme file
    
    Program is basically all wrapped up now.

--- Form1.cs
+++ Form1.cs
@@ -2,34 +2,8 @@ using System;
 using System.Drawing.Text;
 using System.Security.Cryptography;
 
-/*
-
-improvements
- 1. whitespace is only 1 character in the array. the chance of it showing up in a random string
-    could be as low as.. 8.12% if all options are selected. Is that a problem? not enough representation?
-
-    here is a representation chart for each option (again based on all options selected)
-
-    chance at least 1 character from list is represented in random 8 char string (all options selected)-
-    symbol     = 96.26% representation
-    alphaLower = 92.26% representation
-    alphaUpper = 92.26% representation
-    numeric    = 58.93% representation
-    whitespace = 08.12% representation 
-
-    should all sets be equal in probability? 
-    there is a representation skew specifically around whitespace and numeric
-    could be solved by duplicating data within these pools until they are around the same size as the other pools
-    but is that a good idea?
-
-    issue with having more common whitespace.. if it triggers back to back it gets confusing li   ke how many spaces is that?
-    1a. back-to-back whitespace.. p   assword
-*/
-
 namespace RandomStringApp {
-    /// <summary>
-    /// This simple program generates a string by randomly selecting characters from the CharPool array and at a length determined by the user
-    /// </summary>
+
     public partial class Form1 : Form {
 
         //Pool of characters to pick from
@@ -43,15 +17,13 @@ namespace RandomStringApp {
 
         //size = 25 THIS NUMBER IS IMPORTANT do not change
         List<char> whiteSpace = new List<char> { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
-        ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
 
-        List<char> symbols = new List<char>{'~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(',
-            ')', '-', '_', '=', '+', '[', '{', ']', '}', '\\', '|', ',', '<', '.',
-            '>', '/', '?', ';', ':', '\'', '\"' };
+        List<char> symbols = new List<char>{'~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-',
+            '_', '=', '+', '[', '{', ']', '}', '\\', '|', ',', '<', '.', '>', '/', '?', ';', ':', '\'', '\"' };
         
-        List<char> numeric = new List<char>() { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+        List<char> numeric = new List<char>() { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '1', '2',
+            '3', '4', '5', '6', '7', '8', '9', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
 
         //variable to control length of output string
         int length = 8;
@@ -91,15 +63,8 @@ namespace RandomStringApp {
                 CharPool.AddRange(whiteSpace);
             }
 
-            //'Randomly' select character from pool and add to string
-            //SUPER RANDOM condition to not allow the program to run if the count is 25 seems very out of place
-            //BUT it isn't. please read this comment
-            //this is condition is a flag that tells the program the charpool is filled with whitespace
-            //if the program tries to generate a random string with only whitespace it will crash
-            //the size of the whitespace pool is 25
-            //the rest of the pools are of different sizes and no combination of the other pools will ever = 25
-            //it is a clever, efficient, and.. imperfect solution
-            if (CharPool.Count < 1 || CharPool.Count == 25) {
+            //'Randomly' select a char from the pool and add to string
+            if (CharPool.Count < 1 || CharPool.Count == 25) { //can't generate string from empty pool; can't generate string from pool of whitespace (the whitespace array is size 25 specifically as a flag value)
                 errorLabel.Visible = true;
             }
             else {
